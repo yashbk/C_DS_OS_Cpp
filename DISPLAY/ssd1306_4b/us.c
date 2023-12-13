e97b9a5909cc24c93f70668232c9c384229f1c03
Yashas B K <yashas.bk@thundersoft.com>
2023-12-13 13:51:00 +0000

    Done

--- DISPLAY/ssd1306_4b/us.c
+++ DISPLAY/ssd1306_4b/us.c
@@ -25,33 +25,9 @@ int main()
 			close(fd);
 			return -1;
 		}
-		else
-			printf("The written data is %s\n",write_data);
-
-		if(read(fd,buffer,sizeof(buffer)-1) == -1)
-		{
-			printf("Device read failed\n");
-			close(fd);
-			return -1;
-		}
-		else
-			printf("The read data is %s\n",buffer);
 	}
 
 	close(fd);
-
-	printf("Opening multiple times\n");
-	int fds[5];
-	for(int i=0;i<5;i++)
-	{
-		fds[i] = open("/dev/bk_device",O_RDWR);
-		printf("fds[%d] = %d\n",i,fds[i]);
-	}
-
-	for(int i=0;i<5;i++)
-	{
-		close(fds[i]);
-	}
 	return 0;
 }
 
