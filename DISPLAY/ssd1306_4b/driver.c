04522a73cbcbeca2b17512c43fb6383b8e57396a
Yashas <yashas.bk@thundersoft.com>
2023-12-13 18:37:44 +0530

    check

--- DISPLAY/ssd1306_4b/driver.c
+++ DISPLAY/ssd1306_4b/driver.c
@@ -47,6 +47,25 @@ static void SSD1306_GoToNextLine( void );
 static void SSD1306_SetCursor( uint8_t lineNo, uint8_t cursorPos );
 static void SSD1306_Write(bool is_cmd, unsigned char data);
 
+//Function declarations;
+static int my_open(struct inode *inode,struct file *file);
+static int my_release(struct inode *inode, struct file *file);
+static ssize_t my_read(struct file *filp,char __user *buf, size_t len, loff_t *off);
+static ssize_t my_write(struct file *filp,const char __user *buf, size_t len, loff_t *off);
+
+
+dev_t dev_num = 0;
+static struct class *my_class;
+static struct cdev *my_cdev;
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = my_open,
+	.release = my_release,
+	.read = my_read,
+	.write = my_write
+};
+uint8_t * buffer = NULL;
+
 
 static struct i2c_adapter *etx_i2c_adapter     = NULL;  // I2C Adapter Structure
 static struct i2c_client  *etx_i2c_client_oled = NULL;  // I2C Cient Structure (In our case it is OLED)
@@ -554,6 +573,49 @@ static void SSD1306_Fill(unsigned char data)
 static int etx_oled_probe(struct i2c_client *client,
                           const struct i2c_device_id *id)
 {
+
+  	//Assigning major and minor number
+	if(alloc_chrdev_region(&dev_num,0,1,"my_driver")<0)
+		pr_info("allocation failed\n");
+	pr_info("Major number is %d and minor number is %d\n",MINOR(dev_num),MAJOR(dev_num));
+
+	//Creating device file
+	//Creating class
+	if(IS_ERR(my_class = class_create(THIS_MODULE,"my_class_driver")))
+	{
+		pr_err("Class creation failed\n");
+		unregister_chrdev_region(dev_num,1);
+		return -1;
+	}
+
+
+	//Creating device
+	if(IS_ERR(device_create(my_class,NULL,dev_num,NULL,"bk_device")))
+	{
+		pr_err("Device creation failed\n");
+		class_destroy(my_class);
+		return -1;
+	}
+
+
+	my_cdev = cdev_alloc();
+	cdev_init(my_cdev,&fops);
+	
+	if((cdev_add(my_cdev,dev_num,1)<0))
+	{
+		pr_err("Cannot link cdev to VFS\n");
+		device_destroy(my_class,dev_num);
+		return -1;
+	}
+
+
+	if(buffer = kmalloc(1024,GFP_KERNEL) == NULL)
+	{
+		pr_info("Buffer creation failed\n");
+	}
+
+	strcpy(buffer,"Hello yashas");
+
   SSD1306_DisplayInit();
   
   //Set cursor
@@ -620,6 +682,42 @@ static struct i2c_driver etx_oled_driver = {
 static struct i2c_board_info oled_i2c_board_info = {
     I2C_BOARD_INFO(SLAVE_DEVICE_NAME, SSD1306_SLAVE_ADDR)
 };
+
+static int my_open(struct inode *inode,struct file *file)
+{
+	pr_info("Driver open\n");
+	return 0;
+}
+
+static int my_release(struct inode *inode,struct file *file)
+{
+	pr_info("Driver close\n");
+	return 0;
+}
+
+static ssize_t my_read(struct file *filp,char __user *buf, size_t len, loff_t *off)
+{
+	return 0;
+}
+
+static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+	if(copy_from_user(buffer,buf,len))
+	{
+		pr_err("Could not write to kernel memory\n");
+	pr_info("Driver write function called\n");
+
+  SSD1306_DisplayInit();
+  
+  //Set cursor
+  SSD1306_SetCursor(0,0);  
+  SSD1306_StartScrollHorizontal( true, 0, 2);
+
+  //Write String to OLED
+  SSD1306_String("Good\nmorning\nBoi\n\n");
+
+	return 0;
+}
  
 /*
 ** Module Init function
@@ -651,6 +749,11 @@ static int __init etx_driver_init(void)
 */
 static void __exit etx_driver_exit(void)
 {
+  kfree(buffer);
+	cdev_del(my_cdev);
+	device_destroy(my_class,dev_num);
+	class_destroy(my_class);
+	unregister_chrdev_region(dev_num,1);
   i2c_unregister_device(etx_i2c_client_oled);
   i2c_del_driver(&etx_oled_driver);
   pr_info("Driver Removed!!!\n");
